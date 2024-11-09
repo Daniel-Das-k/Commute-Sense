0a86d8f514834554b9d02f0471dac1d8fe8cd6e0
Daniel-Das-k <143825117+Daniel-Das-k@users.noreply.github.com>
2024-11-09 06:39:09 +0530

    ml working

---
+++ drowsy.py
@@ -0,0 +1,16 @@
+import torch
+import numpy
+
+import cv2
+from PIL import Image, ImageTk
+import vlc
+
+import tkinter as tk
+import customtkinter as ctk
+
+app = tk.Tk()
+app.geometry("600x600")
+app.title("Drowsy")
+ctk.set_appearance_mode("dark")
+
+app.mainloop()
